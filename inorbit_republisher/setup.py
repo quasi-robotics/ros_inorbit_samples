26762fcc91b098215da3b5493fa098fce9635af1
Quasi Robotics <info@quasi.ai>
2026-04-14 14:33:10 +0000

    adjusted package installation

--- inorbit_republisher/setup.py
+++ inorbit_republisher/setup.py
@@ -6,22 +6,22 @@ package_name = 'inorbit_republisher'
 
 setup(
     name=package_name,
-    version='0.4.1',
+    version='0.4.2',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*')),
-        (os.path.join('share', package_name, 'config'), glob('config/*'))
+        ('share/' + package_name + '/launch', ['launch/inorbit_republisher.launch.py']),
+        ('share/' + package_name + '/config', ['config/inorbit_republisher.yaml']),
     ],
+    package_data={'': ['py.typed']},
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='InOrbit',
     maintainer_email='support@inorbit.ai',
     description='ROS2 to InOrbit topic republisher',
     license='MIT',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'republisher = inorbit_republisher.republisher:main'
